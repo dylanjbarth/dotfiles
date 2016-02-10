2948b0f6f6320930edf58adfdf9160ca3d8bc872
Dylan Barth <dylan@growthintel.com>
2016-02-10 16:57:34 +0000

    remove desk configs

--- .desk/desks/api.sh
+++
@@ -1,5 +0,0 @@
-# Description: hack on the api.
-
-echo "Entering api"
-cd ~/hacking/growthintel/api
-
--- .desk/desks/prash_dash_home.sh
+++
@@ -1,5 +0,0 @@
-# Description: desk for working on Prash dash on home mac
-
-cd ~/Hacking/prash_dash
-
-echo "In prash dash dir"
